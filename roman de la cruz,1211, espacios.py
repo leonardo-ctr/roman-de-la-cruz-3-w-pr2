e46b4d450c445238ed6872d7730de6e654c1f4bb
pollocandente123 <a.23308051281211@cbtis128.edu.mx>
2024-10-09 19:59:18 -0600

    Update roman de la cruz,1211, espacios.py

--- roman de la cruz,1211, espacios.py
+++ roman de la cruz,1211, espacios.py
@@ -1,3 +1,5 @@
+print(" ") #DEFINE UNA LINEA EN BLANCO
+print("Roman De la Cruz Leonardo Antonio,3-w, 1211 ")
 def longitud_ultima_palabra(texto):
     # Eliminar espacios en blanco al principio y al final, y dividir el texto en palabras
     palabras = texto.strip().split()
@@ -5,8 +7,13 @@ def longitud_ultima_palabra(texto):
     if palabras:
         return len(palabras[-1])  # Devolver la longitud de la última palabra
     return 0  # Si no hay palabras, devolver 0
-
+print(" ") #DEFINE UNA LINEA EN BLANCO
 # Ejemplo de uso
+print(" ") #DEFINE UNA LINEA EN BLANCO
 texto = "   Hola mundo   "
-longitud = longitud_ultima_palabra(texto)
+print(" ") #DEFINE UNA LINEA EN BLANCO
+longitud = longitud_ultima_palabra(texto)#variable
+print(" ") #DEFINE UNA LINEA EN BLANCO
 print(f"La longitud de la última palabra es: {longitud}")
+#imprime resultado
+print(" ") #DEFINE UNA LINEA EN BLANCO
