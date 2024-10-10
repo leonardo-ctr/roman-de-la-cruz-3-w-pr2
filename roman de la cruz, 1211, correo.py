875ba0b940a6867748edfd103fc386e8dc3fd53c
pollocandente123 <a.23308051281211@cbtis128.edu.mx>
2024-10-09 19:57:53 -0600

    Update roman de la cruz, 1211, correo.py

--- roman de la cruz, 1211, correo.py
+++ roman de la cruz, 1211, correo.py
@@ -1,11 +1,15 @@
+print(" ")#define una linea en blanco
+print("Roman De la Cruz Leonardo Antonio, numero")
 def es_email_valido(email):
     return '@' in email
-
+print(" ")#define una linea en blanco
 # Capturar dirección de email
 email_usuario = input("Introduce tu dirección de email: ")
-
+print(" ")#define una linea en blanco
 # Verificar si es válida
 if es_email_valido(email_usuario):
     print("La dirección de email es válida.")
+    print(" ")#define una linea en blanco
 else:
     print("La dirección de email no es válida.")
+    print(" ")#define una linea en blanco
