e2e93518a7338859de5597c85f6e0f7ba1ad750e
pollocandente123 <a.23308051281211@cbtis128.edu.mx>
2024-10-09 20:00:17 -0600

    Update roman de la cruz,1211, operaciones.py

--- roman de la cruz,1211, operaciones.py
+++ roman de la cruz,1211, operaciones.py
@@ -1,4 +1,7 @@
+print(" ")#define una linea en blanco
+print("Roman De la Cruz Leonardo Antonio, numero")
 def sum(lista):
+    #operaciones y valores
     total = 0
     for numero in lista:
         total += numero
@@ -13,8 +16,8 @@ def multip(lista):
 # Ejemplo de uso
 numeros = [1, 2, 3, 4]
 
-resultado_suma = sum(numeros)
+resultado_suma = sum(numeros)#resultado de los datos
 resultado_multiplicacion = multip(numeros)
 
-print(f"La suma de la lista es: {resultado_suma}")         # Debería devolver 10
-print(f"La multiplicación de la lista es: {resultado_multiplicacion}")  # Debería devolver 24
+print(f"La suma de la lista es: {resultado_suma}")  # Debería devolver 10
+print(f"La multiplicación de la lista es: {resultado_multiplicacion}") # Debería devolver 24
