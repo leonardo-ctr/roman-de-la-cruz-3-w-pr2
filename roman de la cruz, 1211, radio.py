63cabf7922c628cdf50fb2dc56b99d1d1c192853
pollocandente123 <a.23308051281211@cbtis128.edu.mx>
2024-10-09 19:58:44 -0600

    Update roman de la cruz, 1211, radio.py

--- roman de la cruz, 1211, radio.py
+++ roman de la cruz, 1211, radio.py
@@ -1,21 +1,27 @@
+print(" ")#define una linea en blanco
+print("Roman De la Cruz Leonardo Antonio,3.w,1211")
 import math
-
+print(" ")#define una linea en blanco
+#solicita valores
+print(" ")#define una linea en blanco
 def calcular_area_circulo(radio):
     return math.pi * (radio ** 2)
-
+print(" ")#define una linea en blanco
+#solicita valores
 def calcular_volumen_cilindro(radio, altura):
     area_base = calcular_area_circulo(radio)  # Usamos la función anterior
     volumen = area_base * altura
     return volumen
-    
-    # Datos de entrada
+    print(" ")#define una linea en blanco
+# Datos de entrada
 radio = 5  # Radio del círculo
 altura = 10  # Altura del cilindro
-
+print(" ")#define una linea en blanco
 # Calcular el área del círculo
 area = calcular_area_circulo(radio)
 print(f"El área del círculo es: {area:.2f}")
-
+print(" ")#define una linea en blanco
 # Calcular el volumen del cilindro
 volumen = calcular_volumen_cilindro(radio, altura)
 print(f"El volumen del cilindro es: {volumen:.2f}")
+print(" ")#define una linea en blanco
