033f3f1f35f00c554289f66f2fdda828ab652aea
pollocandente123 <a.23308051281211@cbtis128.edu.mx>
2024-10-09 19:59:37 -0600

    Update roman de la cruz,1211, funcion true.py

--- roman de la cruz,1211, funcion true.py
+++ roman de la cruz,1211, funcion true.py
@@ -1,8 +1,13 @@
+print(" ") #DEFINE UNA LINEA EN BLANCO
+print("Roman De la Cruz Leonardo Antonio,3-w, 1211 ")
 def es_vocal(caracter):
+    print(" ") #DEFINE UNA LINEA EN BLANCO
     # Convertir el carácter a minúsculas para hacer la comparación
     return caracter.lower() in 'aeiou'
+print(" ") #DEFINE UNA LINEA EN BLANCO
 
 # Ejemplo de uso
 caracter = 'a'
 resultado = es_vocal(caracter)
 print(f"¿'{caracter}' es una vocal? {resultado}")  # Debería devolver True
+print(" ") #DEFINE UNA LINEA EN BLANCO
