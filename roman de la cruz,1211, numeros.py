f3cd511cb926a6aad3d952612be5d49f06a648c9
pollocandente123 <a.23308051281211@cbtis128.edu.mx>
2024-10-09 20:00:00 -0600

    Update roman de la cruz,1211, numeros.py

--- roman de la cruz,1211, numeros.py
+++ roman de la cruz,1211, numeros.py
@@ -1,10 +1,13 @@
+print(" ")#define una linea en blanco
+print("Roman De la Cruz Leonardo Antonio, numero")
 def mayor_de_tres(num1, num2, num3):
     return max(num1, num2, num3)
-
+print(" ")#define una linea en blanco
 # Ejemplo de uso
-numero1 = 10
-numero2 = 25
-numero3 = 15
-
+numero1 = 100
+numero2 = 54
+numero3 = 43
+print(" ")#define una linea en blanco
 mayor = mayor_de_tres(numero1, numero2, numero3)
 print(f"El mayor de los tres números es: {mayor}")
+print(" ")#define una linea en blanco
